bda74fe2713ffc83f9b859ac01fd8b1159f8ffd8
Senne <senne.brusselaers@student.ap.be>
2025-04-28 15:38:10 +0200

    refactor: Comment out unused movement and rotation code in CubeAgent

--- Assets/CubeAgent.cs
+++ Assets/CubeAgent.cs
@@ -60,17 +60,17 @@ public class CubeAgent : Agent
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         // Continuous actions for movement and rotation
-        float forwardAction = actionBuffers.ContinuousActions[0]; // Forward/Backward
-        float sideAction = actionBuffers.ContinuousActions[1]; // Left/Right
+        //float forwardAction = actionBuffers.ContinuousActions[0]; // Forward/Backward
+        //float sideAction = actionBuffers.ContinuousActions[1]; // Left/Right
         //float rotationAction = actionBuffers.ContinuousActions[2]; // Rotation
 
         // Discrete action for jump
         int jumpAction = actionBuffers.DiscreteActions[0];
 
-        Vector3 controlSignal = new Vector3(sideAction, 0, forwardAction);
+        //Vector3 controlSignal = new Vector3(sideAction, 0, forwardAction);
 
         // Apply movement
-        transform.Translate(controlSignal * speedMultiplier);
+        //transform.Translate(controlSignal * speedMultiplier);
 
         // Apply rotation
         //transform.Rotate(Vector3.up, rotationAction * 90 * Time.deltaTime);
@@ -107,8 +107,8 @@ public class CubeAgent : Agent
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Vertical"); // Forward/Backward
+        //var continuousActionsOut = actionsOut.ContinuousActions;
+        //continuousActionsOut[0] = Input.GetAxis("Vertical"); // Forward/Backward
         //continuousActionsOut[1] = Input.GetAxis("Horizontal"); // Left/Right
         //continuousActionsOut[2] = Input.GetAxis("Mouse X"); // Rotation
 
